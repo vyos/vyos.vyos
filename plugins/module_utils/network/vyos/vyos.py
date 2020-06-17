79a1a31ec5a2c2f682f5fd9958b5ff8c8498c1b9
Rohit Thakur <rohitthakur2590@outlook.com>
2020-06-17 17:17:01 +0530

    sanity test fixes
    Signed-off-by: Rohit Thakur <rohitthakur2590@outlook.com>

--- plugins/module_utils/network/vyos/vyos.py
+++ plugins/module_utils/network/vyos/vyos.py
@@ -25,6 +25,7 @@
 # LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
 # USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
+# pylint: skip-file
 import json
 
 from ansible.module_utils._text import to_text
