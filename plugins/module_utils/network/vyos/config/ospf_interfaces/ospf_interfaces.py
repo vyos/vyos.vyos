3cc5cf9c2d018495b88cd315c21798cd8cfd395d
GomathiselviS <gomathiselvi@gmail.com>
2021-02-17 12:50:09 -0500

    removed dict comprehension

--- plugins/module_utils/network/vyos/config/ospf_interfaces/ospf_interfaces.py
+++ plugins/module_utils/network/vyos/config/ospf_interfaces/ospf_interfaces.py
@@ -92,9 +92,11 @@ class Ospf_interfaces(ResourceModule):
 
         # if state is deleted, empty out wantd and set haved to wantd
         if self.state == "deleted":
-            haved = {
-                k: v for k, v in iteritems(haved) if k in wantd or not wantd
-            }
+            h_del = {}
+            for k, v in iteritems(haved):
+                if k in wantd or not wantd:
+                    h_del.update({k: v})
+            haved = h_del 
             have_int = []
             for k, have in iteritems(haved):
                 if k in wantd:
@@ -161,8 +163,8 @@ class Ospf_interfaces(ResourceModule):
     def _ospf_int_list_to_dict(self, entry):
         for name, family in iteritems(entry):
             if "address_family" in family:
-                family["address_family"] = {
-                    entry["afi"]: entry
-                    for entry in family.get("address_family", [])
-                }
+                addr_dict = {}
+                for entry in family.get("address_family", []):
+                    addr_dict.update({entry["afi"]: entry})
+                family["address_family"] = addr_dict
                 self._ospf_int_list_to_dict(family["address_family"])
