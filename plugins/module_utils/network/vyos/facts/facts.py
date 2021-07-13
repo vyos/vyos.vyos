eca9fa2ece2988dbe226c4826e548d1e3ffc27ab
YVarshitha <vyataluru@gmail.com>
2021-07-13 12:24:16 -0400

    add changes to config file

--- plugins/module_utils/network/vyos/facts/facts.py
+++ plugins/module_utils/network/vyos/facts/facts.py
@@ -63,6 +63,7 @@ from ansible_collections.vyos.vyos.plugins.module_utils.network.vyos.facts.prefi
 )
 from ansible_collections.vyos.vyos.plugins.module_utils.network.vyos.facts.logging_global.logging_global import (
     Logging_globalFacts,
+)
 from ansible_collections.vyos.vyos.plugins.module_utils.network.vyos.facts.ntp.ntp import (
     NtpFacts,
 )
@@ -92,7 +93,7 @@ FACT_RESOURCE_SUBSETS = dict(
     route_maps=Route_mapsFacts,
     prefix_lists=Prefix_listsFacts,
     logging_global=Logging_globalFacts,
-    ntp=NtpFacts
+    ntp=NtpFacts,
 )
 
 
