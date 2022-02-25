6623b672656c46785a3515d03277a1cf907d4a08
GomathiselviS <gomathiselvi@gmail.com>
2022-02-25 10:07:19 -0500

    removed q

--- plugins/module_utils/network/vyos/facts/firewall_rules/firewall_rules.py
+++ plugins/module_utils/network/vyos/facts/firewall_rules/firewall_rules.py
@@ -265,9 +265,6 @@ class Firewall_rulesFacts(object):
         """
         a_lst = ["new", "invalid", "related", "established"]
         cfg_dict = self.parse_attr(conf, a_lst, match=attrib)
-        import q
-
-        q(conf, cfg_dict)
         return cfg_dict
 
     def parse_group(self, conf, attrib=None):
