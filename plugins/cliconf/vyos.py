48abed1f0387ca733662b37996ae108b83c9800e
Gaige B. Paulsen <gaige@cluetrust.com>
2024-05-25 05:58:26 -0400

    fix: version check

--- plugins/cliconf/vyos.py
+++ plugins/cliconf/vyos.py
@@ -79,6 +79,11 @@ class Cliconf(CliconfBase):
             match = re.search(r"Version:\s*(.*)", data)
             if match:
                 device_info["network_os_version"] = match.group(1)
+            
+            if device_info["network_os_version"]:
+                match = re.search(r"VyOS\s*(\d+\.\d+)", device_info["network_os_version"])
+                if match:
+                    device_info["network_os_major_version"] = match.group(1)
 
             match = re.search(r"(?:HW|Hardware) model:\s*(\S+)", data)
             if match:
