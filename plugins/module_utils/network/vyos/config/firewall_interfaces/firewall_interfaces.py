67cda0147ab54136ef0604427c5a01a7f4b21986
maxime <37832743+mlk-89@users.noreply.github.com>
2024-11-18 20:37:36 +0100

    Add feature for bonding/vlan interface in the firewall_interfaces (#246)
    * - Add feature for bonding interface in the firewall_interfaces
    - Add feature for vlan interface in the firewall_interfaces
    
    * fix a bug when invoking replaced in the module firewall_rules.
    
    * - Add feature for bonding interface in the firewall_interfaces
    - Add feature for vlan interface in the firewall_interfaces
    
    * test: add tests
    
    * fix: support for interface types
    
    * docs: fixed for 1.4 deprecation
    
    ---------
    
    Co-authored-by: Maxime.L <maxime@nfrance.com>
    Co-authored-by: Gaige B. Paulsen <gaige@cluetrust.com>
    Co-authored-by: Gaige B Paulsen <gaige@cluetrust.net>

--- plugins/module_utils/network/vyos/config/firewall_interfaces/firewall_interfaces.py
+++ plugins/module_utils/network/vyos/config/firewall_interfaces/firewall_interfaces.py
@@ -27,6 +27,9 @@ from ansible_collections.ansible.netcommon.plugins.module_utils.network.common.u
 )
 
 from ansible_collections.vyos.vyos.plugins.module_utils.network.vyos.facts.facts import Facts
+from ansible_collections.vyos.vyos.plugins.module_utils.network.vyos.utils.utils import (
+    get_interface_type,
+)
 
 
 class Firewall_interfaces(ConfigBase):
@@ -393,10 +396,24 @@ class Firewall_interfaces(ConfigBase):
         :param opr: operation flag.
         :return: generated command.
         """
+
+        # Append vif if interface contains a dot
+        vlan = None
+        interface_real = name
+        if "." in name:
+            interface_real, vlan = name.split(".")
+
+        if vlan is not None:
+            interface_real = interface_real + " vif " + vlan
+
+        # if interface name is bondX, then it's a bonding interface. Everything else is an ethernet
+        iftype = get_interface_type(interface_real)
+
         if not opr:
-            cmd = "delete interfaces ethernet" + " " + name + " firewall"
+            cmd = "delete interfaces " + iftype + " " + interface_real + " firewall"
         else:
-            cmd = "set interfaces ethernet" + " " + name + " firewall"
+            cmd = "set interfaces " + iftype + " " + interface_real + " firewall"
+
         if attrib:
             cmd += " " + attrib
         if afi:
