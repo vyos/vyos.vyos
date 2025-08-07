0ab80513534e1cd28fff7eed624c0fb012335e6b
omnom62 <omnom62@outlook.com>
2025-08-07 21:17:20 +1000

    chanelog added

--- plugins/modules/vyos_config.py
+++ plugins/modules/vyos_config.py
@@ -132,6 +132,7 @@ EXAMPLES = """
       - set system host-name {{ inventory_hostname }}
       - set service lldp
       - delete service dhcp-server
+    match: line
 
 - name: backup and load from file
   vyos.vyos.vyos_config:
@@ -147,6 +148,7 @@ EXAMPLES = """
     lines:
       # - set int eth eth2 description 'OUTSIDE'
       - set interface ethernet eth2 description 'OUTSIDE'
+    match: line
 
 - name: configurable backup path
   vyos.vyos.vyos_config:
