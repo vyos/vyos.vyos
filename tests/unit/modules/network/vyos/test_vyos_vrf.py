f885778ba490d3c7ff220963b9f8573b028d1bd9
omnom62 <omnom62@outlook.com>
2026-02-07 21:47:28 +1000

    VRF uat

--- tests/unit/modules/network/vyos/test_vyos_vrf.py
+++ tests/unit/modules/network/vyos/test_vyos_vrf.py
@@ -155,13 +155,138 @@ class TestVyosVrfModule(TestVyosModule):
         set_module_args(
             dict(
                 config=dict(
-                    bind_to_all=True,
+                    bind_to_all=False,
                     instances=[
                         dict(
                             name="vrf-green",
                             description="green-vrf",
                             table_id=110,
                             vni=1010,
+                            protocols=dict(
+                                ospf=dict(
+                                    log_adjacency_changes="detail",
+                                    max_metric=dict(
+                                        router_lsa=dict(
+                                            administrative=True,
+                                            on_shutdown=10,
+                                            on_startup=10,
+                                        ),
+                                    ),
+                                    default_information=dict(
+                                        originate=dict(
+                                            always=True,
+                                            metric=10,
+                                            metric_type=2,
+                                        ),
+                                    ),
+                                    auto_cost=dict(
+                                        reference_bandwidth=2,
+                                    ),
+                                    neighbor=[
+                                        dict(
+                                            neighbor_id="192.0.11.12",
+                                            poll_interval=10,
+                                            priority=2,
+                                        ),
+                                    ],
+                                    redistribute=[
+                                        dict(
+                                            route_type="bgp",
+                                            metric=10,
+                                            metric_type=2,
+                                        ),
+                                    ],
+                                    parameters=dict(
+                                        router_id="192.0.1.1",
+                                        rfc1583_compatibility=True,
+                                        abr_type="cisco",
+                                    ),
+                                    areas=[
+                                        dict(
+                                            area_id="2",
+                                            area_type=dict(
+                                                normal=True,
+                                            ),
+                                            authentication="plaintext-password",
+                                            shortcut="enable",
+                                        ),
+                                        dict(
+                                            area_id="3",
+                                            area_type=dict(
+                                                nssa=dict(
+                                                    set=True,
+                                                ),
+                                            ),
+                                        ),
+                                        dict(
+                                            area_id="4",
+                                            area_type=dict(
+                                                stub=dict(
+                                                    default_cost=20,
+                                                ),
+                                            ),
+                                            network=[
+                                                dict(
+                                                    address="192.0.2.0/24",
+                                                ),
+                                            ],
+                                            range=[
+                                                dict(
+                                                    address="192.0.3.0/24",
+                                                    cost=10,
+                                                ),
+                                                dict(
+                                                    address="192.0.4.0/24",
+                                                    cost=12,
+                                                ),
+                                            ],
+                                        ),
+                                    ],
+                                ),
+                                # static=[
+                                #     dict(
+                                #         address_families=[
+                                #             dict(
+                                #                 afi="ipv4",
+                                #                 routes=[
+                                #                     dict(
+                                #                         dest="192.0.2.0/24",
+                                #                         blackhole_config=dict(
+                                #                             distance=10,
+                                #                         ),
+                                #                         next_hops=[
+                                #                             dict(
+                                #                                 interface="eth2",
+                                #                             ),
+                                #                         ],
+                                #                     ),
+                                #                     dict(
+                                #                         dest="10.10.10.0/24",
+                                #                         blackhole_config=dict(),
+                                #                         next_hops=[],
+                                #                     ),
+                                #                 ],
+                                #             ),
+                                #             dict(
+                                #                 afi="ipv6",
+                                #                 routes=[
+                                #                     dict(
+                                #                         dest="2001:db8::/32",
+                                #                         blackhole_config=dict(
+                                #                             distance=20,
+                                #                         ),
+                                #                         next_hops=[
+                                #                             dict(
+                                #                                 interface="eth2",
+                                #                             ),
+                                #                         ],
+                                #                     ),
+                                #                 ],
+                                #             ),
+                                #         ],
+                                #     ),
+                                # ],
+                            ),
                         ),
                     ],
                 ),
@@ -170,9 +295,40 @@ class TestVyosVrfModule(TestVyosModule):
         )
 
         commands = [
+            "delete vrf bind-to-all",
             "set vrf name vrf-green table 110",
             "set vrf name vrf-green vni 1010",
             "set vrf name vrf-green description green-vrf",
+            "set vrf name vrf-green protocols ospf log-adjacency-changes 'detail'",
+            "set vrf name vrf-green protocols ospf max-metric router-lsa administrative",
+            "set vrf name vrf-green protocols ospf max-metric router-lsa on-shutdown 10",
+            "set vrf name vrf-green protocols ospf max-metric router-lsa on-startup 10",
+            "set vrf name vrf-green protocols ospf default-information originate always",
+            "set vrf name vrf-green protocols ospf default-information originate metric 10",
+            "set vrf name vrf-green protocols ospf default-information originate metric-type 2",
+            "set vrf name vrf-green protocols ospf auto-cost reference-bandwidth '2'",
+            "set vrf name vrf-green protocols ospf neighbor 192.0.11.12",
+            "set vrf name vrf-green protocols ospf neighbor 192.0.11.12 poll-interval 10",
+            "set vrf name vrf-green protocols ospf neighbor 192.0.11.12 priority 2",
+            "set vrf name vrf-green protocols ospf redistribute bgp",
+            "set vrf name vrf-green protocols ospf redistribute bgp metric 10",
+            "set vrf name vrf-green protocols ospf redistribute bgp metric-type 2",
+            "set vrf name vrf-green protocols ospf parameters router-id '192.0.1.1'",
+            "set vrf name vrf-green protocols ospf parameters rfc1583-compatibility",
+            "set vrf name vrf-green protocols ospf parameters abr-type 'cisco'",
+            "set vrf name vrf-green protocols ospf area '2'",
+            "set vrf name vrf-green protocols ospf area 2 area-type normal",
+            "set vrf name vrf-green protocols ospf area 2 authentication plaintext-password",
+            "set vrf name vrf-green protocols ospf area 2 shortcut enable",
+            "set vrf name vrf-green protocols ospf area '3'",
+            "set vrf name vrf-green protocols ospf area 3 area-type nssa",
+            "set vrf name vrf-green protocols ospf area '4'",
+            "set vrf name vrf-green protocols ospf area 4 area-type stub default-cost 20",
+            "set vrf name vrf-green protocols ospf area 4 network 192.0.2.0/24",
+            "set vrf name vrf-green protocols ospf area 4 range 192.0.3.0/24",
+            "set vrf name vrf-green protocols ospf area 4 range 192.0.3.0/24 cost 10",
+            "set vrf name vrf-green protocols ospf area 4 range 192.0.4.0/24",
+            "set vrf name vrf-green protocols ospf area 4 range 192.0.4.0/24 cost 12",
         ]
 
         self.execute_module(changed=True, commands=commands)
@@ -277,6 +433,24 @@ class TestVyosVrfModule(TestVyosModule):
                             table_id=103,
                             vni=1002,
                         ),
+                        dict(
+                            name="vrf-red",
+                            description="red-vrf",
+                            disable=True,
+                            table_id=101,
+                            vni=1001,
+                            protocols=dict(
+                                bgp=dict(
+                                    as_number=65000,
+                                    neighbor=[
+                                        dict(
+                                            address="192.0.2.1",
+                                            remote_as=65003,
+                                        ),
+                                    ],
+                                ),
+                            ),
+                        ),
                     ],
                 ),
                 state="overridden",
@@ -285,10 +459,18 @@ class TestVyosVrfModule(TestVyosModule):
         commands = [
             "delete vrf name vrf-blue",
             "commit",
+            "delete vrf name vrf-red",
+            "commit",
             "set vrf name vrf-blue table 103",
             "set vrf name vrf-blue vni 1002",
             "set vrf name vrf-blue description blue-vrf",
             "set vrf name vrf-blue disable",
+            "set vrf name vrf-red table 101",
+            "set vrf name vrf-red vni 1001",
+            "set vrf name vrf-red description red-vrf",
+            "set vrf name vrf-red disable",
+            "set vrf name vrf-red protocols bgp system-as 65000",
+            "set vrf name vrf-red protocols bgp neighbor 192.0.2.1 remote-as 65003",
             "delete vrf bind-to-all",
         ]
         self.execute_module(changed=True, commands=commands)
