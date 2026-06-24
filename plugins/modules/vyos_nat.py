8d6327d606caae39db481d4fd5217595b702b608
omnom62 <omnom62@outlook.com>
2026-06-25 06:27:42 +1000

    T8323: copyright headers fixed

--- plugins/modules/vyos_nat.py
+++ plugins/modules/vyos_nat.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# Copyright 2024 Red Hat
-# GNU General Public License v3.0+
-# (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 """
 The module file for vyos_nat
